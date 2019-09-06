9404bf71751c7dd0e7830f80c2af261c8290f67e
Isuru Fernando <isuruf@gmail.com>
2019-09-05 22:49:19 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,12 @@
 
 chmod +x configure
 
-if [ "$(uname)" == "Darwin" ];
-then
+if [[ "$target_platform" == "osx-64" ]]; then
     ./configure --prefix=$PREFIX --with-pic --enable-cxx --build=x86_64-apple-darwin
-else
+elif  [[ "$target_platform" == "linux-64" ]]; then
     ./configure --prefix=$PREFIX --with-pic --enable-cxx --enable-fat
+else
+    ./configure --prefix=$PREFIX --with-pic --enable-cxx --host=$HOST
 fi
 
 make -j${CPU_COUNT}
